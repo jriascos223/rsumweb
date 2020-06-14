1f004c5dedf774c9d77607563a0200226cbafac6
Joseph Riascos <josephriascos@gmail.com>
2020-06-14 15:31:41 -0400

    the bar graph works perfectly now, although no user input can be accepted yet. May not even be possible, given the time constraint I gave myself :(

--- script/util.js
+++ script/util.js
@@ -1,40 +1,89 @@
 var ctx = document.getElementById("myChart").getContext("2d");
 var data = {
-    labels: [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    datasets: [/* {
-        label: "f(x) = x", // Name it as you want
-        function: function(x) { return x },
-        data: [], // Don't forget to add an empty data array, or else it will break
-        borderColor: "rgba(75, 192, 192, 1)",
-        fill: false,
-    }, */
+    labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    datasets: [
     {
-        label: "fucking bar graph", // Name it as you want
-        function: function(x) { return x },
-        data: [], // Don't forget to add an empty data array, or else it will break
-        backgroundColor: [],
-        type: "bar"
-    },
-    {
-        label: "f(x) = x²",
-        function: function(x) { return x*x },
+        label: "f(x) = x",
+        function: polynomial,
         data: [],
         borderColor: "rgba(153, 102, 255, 1)",
-        fill: false
+        fill: false,
+        type: "line"
     },
     {
-        label: "f(x) = x * log(x)",
-        function: function(x) {return x*Math.log(x)},
-        borderColor: "rgba(225, 206, 86, 1)",
+        label: "f(x) = x",
+        function: polynomial,
         data: [],
-        fill: false
+        backgroundColor: ["rgba(0, 255, 0, 0.5)"],
+        fill: false,
+        type: "bar"
     }]
+}
+
+function polynomial(x) {
+    return x;
+}
+
+function calculateBarPositions(left, data) {
+    var xPositions = [];
+    var yPositions = [];
+    var output = [];
+    if (left == false) {
+        if (data) {
+            console.log("good to go!");
+            for(var i = 0; i < data.datasets.length; i++) {
+                for (var j = 0; j < data.labels.length; j++) {
+                    xPositions.push(data.labels[j]);
+                    yPositions.push(data.datasets[i].function(data.labels[j]));
+                }
+            }
+            yPositions.splice(yPositions.length / 2, yPositions.length / 2);
+            xPositions.splice(xPositions.length / 2, xPositions.length / 2);
+            xPositions.push(xPositions[xPositions.length - 1] - xPositions[xPositions.length - 2] + xPositions[xPositions.length - 1]);
+            yPositions.push((polynomial(xPositions[xPositions.length -1]) + polynomial(xPositions[xPositions.length -2])) / 2);
+            console.log(yPositions);
+            console.log(xPositions);
+
+            for (var i = 1; i < yPositions.length; i++) {
+                output.push((yPositions[i-1] + yPositions[i])/2);
+            }
+            console.log(output);
+            return output;
+        }
+        
+    }else {
+        console.log("good to go!");
+            for(var i = 0; i < data.datasets.length; i++) {
+                for (var j = 0; j < data.labels.length; j++) {
+                    xPositions.push(data.labels[j]);
+                    yPositions.push(data.datasets[i].function(data.labels[j]));
+                }
+            }
+            yPositions.splice(yPositions.length / 2, yPositions.length / 2);
+            xPositions.splice(xPositions.length / 2, xPositions.length / 2);
+            xPositions.push(xPositions[xPositions.length - 1] - xPositions[xPositions.length - 2] + xPositions[xPositions.length - 1]);
+            yPositions.push((polynomial(xPositions[xPositions.length -1]) + polynomial(xPositions[xPositions.length -2])) / 2);
+            console.log(yPositions);
+            console.log(xPositions);
+
+            for (var i = 0; i < yPositions.length; i++) {
+                if (i == 0) {
+                    output.push(0);
+                    continue;
+                }
+                output.push((yPositions[i-1] + yPositions[i])/2);
+            }
+            console.log(output);
+            return output;
+    }
+    
 }
 
 Chart.pluginService.register({
     beforeInit: function(chart) {
         // We get the chart data
         var data = chart.config.data;
+        var yBarPositions = calculateBarPositions(false, data);
 
         // For every dataset ...
         for (var i = 0; i < data.datasets.length; i++) {
@@ -43,12 +92,19 @@ Chart.pluginService.register({
             for (var j = 0; j < data.labels.length; j++) {
 
                 // We get the dataset's function and calculate the value
-                var fct = data.datasets[i].function,
+                if(data.datasets[i].type !== "bar") {
+                    var fct = data.datasets[i].function,
                     x = data.labels[j],
                     y = fct(x);
-                // Then we add the value to the dataset data
-                data.datasets[i].data.push(y);
-                console.log(data.datasets[i].backgroundColor);
+                    // Then we add the value to the dataset data
+                    data.datasets[i].data.push(y);
+                }else if (data.datasets[i].type == "bar") {
+                    data.datasets[i].data = yBarPositions;
+                }
+                
+                if (data.datasets[i].backgroundColor !== undefined) {
+                    data.datasets[i].backgroundColor.push(data.datasets[i].backgroundColor[0]);
+                }
             }
         }
     }
